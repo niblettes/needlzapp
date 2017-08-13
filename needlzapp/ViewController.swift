9c1f186ba0560a4851e718e82b47429950b81210
scoobydoo <scoobydoo@scoobydoos-macbook-pro.local>
2017-08-12 18:06:23 -0700

    Initial Commit

---
+++ needlzapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  needlzapp
+//
+//  Created by scoobydoo on 08-12-2017.
+//  Copyright © 2017 Spire Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
